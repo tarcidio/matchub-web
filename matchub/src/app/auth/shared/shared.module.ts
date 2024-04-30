2c65c1fe99e1fbfd998d3e7d6acf3214737bc185
Tarcídio Antônio Júnior <50781629+tarcidio@users.noreply.github.com>
2024-04-30 10:57:52 -0300

    Last visual update

--- matchub/src/app/auth/shared/shared.module.ts
+++ matchub/src/app/auth/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthFormComponent } from './auth-form/auth-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AuthFormComponent],
+  declarations: [AuthFormComponent, FooterComponent],
   imports: [CommonModule],
   //exports é importante para permitir que componentes de outro módulos
   // possam utilizar AuthFormComponent e não apenas componentes deste módulo
-  exports: [AuthFormComponent],
+  exports: [AuthFormComponent, FooterComponent],
 })
 export class SharedModule {}
